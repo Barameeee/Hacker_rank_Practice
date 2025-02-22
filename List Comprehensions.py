9f329b6556fa007c798a06e2c8bd42bc72cd8ae3
Barameeee <166025917+Barameeee@users.noreply.github.com>
2025-02-22 10:45:07 +0000

    list

---
+++ List Comprehensions.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+lists_i = []
+for i in range (0,x+1):
+    lists_i.append(i)
+lists_j = []
+lists_k = []
