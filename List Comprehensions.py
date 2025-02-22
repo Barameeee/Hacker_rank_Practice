490007fa73e552313fe99d097ae0290dfa546036
Barameeee <166025917+Barameeee@users.noreply.github.com>
2025-02-22 11:01:25 +0000

    done

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,11 +1,13 @@
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-    
-lists_i = []
-for i in range (0,x+1):
-    lists_i.append(i)
-lists_j = []
-lists_k = []
+    x = int(input('x = '))
+    y = int(input('y = '))
+    z = int(input('z = '))
+    n = int(input('n = '))
+
+list_v = []
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i+j+k != n:
+                list_v.append([i,j,k])
+print(list_v)
