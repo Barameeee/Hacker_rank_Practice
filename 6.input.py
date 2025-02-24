da5823e8e23ac5176e01378c28abec367bf1fc67
Barameeee <166025917+Barameeee@users.noreply.github.com>
2025-02-24 15:11:33 +0000

    done

---
+++ 6.input.py
@@ -0,0 +1,11 @@
+'''
+x,y = (input().split())
+x = float(x)
+P = 'x**3 + x**2 + x + 1'
+print (eval ('(x**3) + (x**2) + x + 1'))
+'''
+x,k = (input().split())
+x = float(x)
+k = float(k)
+P = input()
+print ((eval (P)) == k)
